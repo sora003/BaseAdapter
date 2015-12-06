1925bb67a68a88544f387e0f3f4a5c59dc384502
sora003 <sumisoras@163.com>
2015-12-06 23:03:14 +0800

    Added ListView声明，布局

---
+++ BaseAdapter/app/src/main/java/com/sora/baseadapter/Bean.java
@@ -0,0 +1,46 @@
+package com.sora.baseadapter;
+
+/**
+ * Created by Sora on 2015/12/6.
+ */
+public class Bean {
+    private String title;
+    private String desc;
+    private String time;
+    private String phone;
+
+    public Bean() {
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
